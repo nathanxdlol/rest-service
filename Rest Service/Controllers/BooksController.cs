999c039260710a4f5ed136a2c62e95dd1bad9b51
nathanxdlol <72072972+nathanxdlol@users.noreply.github.com>
2021-10-14 22:33:10 +0200

    Add files via upload

---
+++ Rest Service/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestService
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksManager _manager = new BooksManager();
+        // get api books controller
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+        [HttpGet("{ISPN13}")]
+        public Book Get(string ISPN13)
+        {
+            return _manager.GetByISBN13(ISPN13);
+        }
+        // post api books controller
+        [HttpPost]
+        public Book Post( Book value)
+        {
+            return _manager.Add(value);
+        }
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{ISPN13}")]
+        public Book Delete(string ISPN13)
+        {
+            return _manager.Delete(ISPN13);
+        }
+    }
+}
---
+++ Rest Service/Managers/BookManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestService
+{
+    public class BookManager
+    {
+        public static List<Book> Data = new List<Book>
+        {
+            new Book {Title = "xxx",Author = "Kuba",PageNumber = 1,ISBN13 = "0", },
+            new Book {Title = "yyy",Author = "Piotr",PageNumber = 2 ,ISBN13 = "1", },
+            new Book {Title = "yyy",Author = "Nathan",PageNumber = 3,ISBN13 = "2", },
+            new Book {Title = "zzz",Author = "Jakub",PageNumber = 4,ISBN13 = "3", },
+        };
+        public Book Add(Book newBook)
+        {
+            Data.Add(newBook);
+            return newBook;
+        }
+        public Book Update(string ISBN13, Book updates)
+        {
+
+            book.Title = updates.Title;
+            book.PageNumber = updates.PageNumber;
+            book.Author = updates.Author;
+            Book book = Data.Find(book1 => book1.ISBN13.Contains(ISBN13));
+            if (book == null) return null;
+            return book;
+        }
+        public Book Delete(string ISBN13)
+        {
+            Book book = Data.Find(book1 => book1.ISBN13.Contains(ISBN13));
+            if (book == null) return null;
+            Data.Remove(book);
+            return book;
+        }
+    }
+}
+
